acb9f8ef0f18f544899fa6eefcee7adef39d7ed3
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-04-03 07:14:24 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+
+const newHeader = document.createElement('h1');{
+newHeader.id = 'victory';
+newHeader.textContent = '';
+
+''
+}
+
+
+newHeader.innerHTML= "YOUR-NAME is the champion";
+
+document.body.appendChild(newHeader);
